54cfc51b4e67cac9964e2ff3b16e175b5b058237
Nikita Orlenko <nikita.orlenko234@gmail.com>
2025-10-06 10:41:49 +0300

    Lõin Index IX_DimEmployee_BaseRate

---
+++ IndexidServeris.sql
@@ -0,0 +1,8 @@
+USE AdventureWorksDW2019;
+
+SELECT * FROM DimEmployee
+
+CREATE INDEX IX_DimEmployee_BaseRate
+ON DimEmployee(BaseRate ASC)
+EXEC sp_help DimEmployee;
+DROP INDEX DimEmployee.IX_DimEmployee_BaseRate
