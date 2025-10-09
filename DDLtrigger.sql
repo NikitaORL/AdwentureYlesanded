93e9c7fe3fdef6d04f9d5c7e4bf1e01a4929c31a
«Nikita <nikita.orlenko234@gmail.com>
2025-10-09 15:26:30 +0300

    Tegin  trigger, mis esmalt teavitab tabeli loomisest, muutmisest või kustutamisest ning seejärel muudab seda, et blokeerida need toimingud ja kuvada teade nende keelustamise kohta

--- DDLtrigger.sql
+++ DDLtrigger.sql
@@ -13,3 +13,24 @@ END
 CREATE TRIGGER trMyFirstTrigger
 ON Database
 FOR CREATE_TABLE
+
+Create Table Test (Id int)
+
+ALTER TRIGGER trMyFirstTrigger
+ON Database
+FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+AS
+BEGIN
+Print 'A table has just been created, modified or deleted'
+END
+
+ALTER TRIGGER trMyFirstTrigger
+
+ON Database
+
+FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+AS
+BEGIN
+Rollback
+Print 'You cannot create, alter or drop a table'
+END
