8cd623d32ec3f8431be5df04308010892df06792
«Nikita <nikita.orlenko234@gmail.com>
2025-10-09 15:38:55 +0300

    Lõin kaks trigerid ja need triggereid blokeerivad tabelite loomise, muutmise ja kustutamise. Üks töötab andmebaasis ja teine töötab serveris

--- DDLtrigger.sql
+++ DDLtrigger.sql
@@ -47,3 +47,26 @@ END
 
 EXEC sp_rename 'TableA', 'TableA_Renamed';
 GO
+-----------------93 fail
+
+
+CREATE TRIGGER tr_DatabaseScopeTrigger
+ON DATABASE
+FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+AS
+BEGIN
+ROLLBACK
+Print 'You cannot create, alter or drop a table in the current database'
+END
+
+
+CREATE TRIGGER tr_ServerScopeTrigger
+ON ALL SERVER
+FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+AS
+BEGIN
+ROLLBACK
+Print 'You cannot create, alter or drop a table in any database on the server'
+END
+
+
