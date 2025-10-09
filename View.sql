8943abe73cd4b50dff96ff0c15234b583823500a
«Nikita <nikita.orlenko234@gmail.com>
2025-10-09 14:35:57 +0300

    Lisan andmed tabeli Department

--- View.sql
+++ View.sql
@@ -12,3 +12,15 @@ create table tblDepartment
 DeptId int primary key,
 DeptName nvarchar(20)
 )
+
+
+Insert into tblDepartment values (1, 'IT')
+Insert into tblDepartment values (2, 'Payroll')
+Insert into tblDepartment values (3,'HR')
+Insert into tblDepartment values (4,'Admin')
+Insert into tblEmployee values (1, 'John', 5000, 'Male', 3)
+Insert into tblEmployee values (2, 'Mike', 3400, 'Male', 2)
+Insert into tblEmployee values (3, 'Pam', 6000, 'Female', 1)
+Insert into tblEmployee values (4, 'Todd', 4800, 'Male', 4)
+Insert into tblEmployee values (5, 'Sara', 3200, 'Female', 1)
+Insert into tblEmployee values (6, 'Ben', 4800, 'Male', 3)
