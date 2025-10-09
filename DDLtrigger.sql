03e3c013c46994aac785826d65f30b7bef0d78aa
«Nikita <nikita.orlenko234@gmail.com>
2025-10-09 15:23:48 +0300

    See on triggerid näided

---
+++ DDLtrigger.sql
@@ -0,0 +1,15 @@
+use AdventureWorksDW2019
+
+
+CREATE TRIGGER [Trigger_Name]
+ON [Scope (Server | Database)]
+FOR [EventType1, EventType2, EventType3, ...],
+AS
+BEGIN
+-- Trigger Body --
+END
+
+
+CREATE TRIGGER trMyFirstTrigger
+ON Database
+FOR CREATE_TABLE
