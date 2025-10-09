1412cd619c70d73597b02200be6cb1f823e24efd
«Nikita <nikita.orlenko234@gmail.com>
2025-10-09 15:15:38 +0300

    Lõin andmed tabelis ja kaks select: esimene näitab TableA k

--- Except.sql
+++ Except.sql
@@ -1,3 +1,14 @@
 use AdventureWorksDW2019
 Select Id, Name, Gender From TableA
 Except Select Id, Name, Gender From TableB
+
+INSERT INTO TableA VALUES (1, 'Mark', 'Male');
+
+SELECT Id, Name, Gender FROM TableA
+EXCEPT
+SELECT Id, Name, Gender FROM TableB;
+
+
+
+Select Id, Name, Gender From TableA Except
+Select Id, Name From TableB
